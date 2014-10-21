dc102315f72c8c7c5c2acd38cab64855e6e92e23
Artiom Tiurin <atiur001@fiu.edu>
2014-10-20 20:01:32 -0400

    work on GitHubJobs API

--- protected/githubJobs/githubJobsapi.php
+++ protected/githubJobs/githubJobsapi.php
@@ -6,9 +6,19 @@
  * source: http://jobs.github.com/api
  */
 class githubJobsapi{
-    const API_URL = "http://jobs.github.com/positions";
+    const API_URL = "http://jobs.github.com/positions.json?";
+    public static $perPage = 25;
+    public static $pageNum = 0;
     
-    private static $API
+    public static function getJobs($keywords, $location)
+    {
+        $location = urlencode($location);
+        $keywords = urldecode($keywords);
+        $url = "http://jobs.github.com/positions.json?callback=myFunc";
+        $json = file_get_contents($url);
+        $data = json_decode($json);
+        echo $data->data[0]->title;
+    }
     
 }
 ?>
